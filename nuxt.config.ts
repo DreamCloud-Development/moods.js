c34b12c0d9805747414b35278b5f2d97efce36c6
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-06-09 20:07:49 +0200

    📱 Added manifest to every pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,8 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'icon', href: "/favicon-dark.svg" }
+        { rel: 'icon', href: "/favicon-dark.svg" },
+        { rel: 'manifest', href: "/manifest.json" }
       ],
       meta: [
         {
