906fc522754d63e231b646bd0f5aa55e9fc5e5f8
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-10 15:47:24 +0200

    📝 Serve nothing, but it's here

---
+++ pages/moods.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-center text-6xl font-black my-8 ">
+            WIP
+        </h1>
+        <SearchBull />
+        <p class="mt-4">Learn more about us ⬇️</p>
+        <NuxtLink class="btn btn-primary" to="/info">About</NuxtLink>
+    </div>
+</template>
