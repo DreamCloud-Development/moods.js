52eae69bfcfeb5e5d6cd1f16292a092f45c85658
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-10 15:31:24 +0200

    📝 Add argument to set placeholder

--- components/SearchBull.vue
+++ components/SearchBull.vue
@@ -2,7 +2,7 @@
     <form @submit.prevent="handleSubmit" class="p-0">
         <label class="input input-sm input-bordered flex items-center gap-1">
             <Icon name="ph:magnifying-glass-bold" />
-            <input type="text" v-model="searchTerm" placeholder={{ placeholderText }} class="placeholder:text-base-content" />
+            <input type="text" v-model="searchTerm" :placeholder=placeholderText class="placeholder:text-base-content" />
         </label>
     </form>
 </template>
