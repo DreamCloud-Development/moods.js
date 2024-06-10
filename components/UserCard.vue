1049ca3d68f5a4126739ca1b57d8f023c876467a
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-10 20:48:42 +0200

    🐛 Fix user search

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card card-side bg-base-300 border-2 border-primary-content mt-2">
         <figure class="hidden lg:flex">
-            <img class="h-32 w-32 rounded-l-none hidden lg:flex" :src="userParsedData.cover_photo['640x']"
+            <img class="h-32 w-32 rounded-l-none hidden lg:flex" :src="userParsedData.profile_picture['480x480']"
                 alt="Cover Image" />
         </figure>
         <div class="card-body col-span-2">
--- pages/search.vue
+++ pages/search.vue
@@ -5,7 +5,7 @@
             Search results for: {{ $route.query.query }}
         </h1>
         <div class="mx-2 lg:mx-10">
-            <div role="tablist" class="tabs tabs-bordered tabs-lg font-bold mt-4 w-full">
+            <div role="tablist" class="tabs lg:tabs-lg font-bold mt-4 w-full">
 
                 <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Tracks" checked />
                 <div role="tabpanel" class="tab-content" v-if="tracks.length != 0" tab-active>
@@ -21,18 +21,27 @@
 
                 <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Albums" />
                 <div role="tabpanel" class="tab-content" v-if="playlists.length != 0">
-                    <p><br>This one work.<br><br>But Audius Not.</p>
+                    <p><br>This feature should work.<br><br>But Audius Not.</p>
                     <div v-for="playlist in playlists">
                         <PlaylistCard v-if="playlist.is_album" :trackParsedData=playlist />
+                    </div>
+                    <div class="collapse collapse-arrow bg-base-200 mt-4">
+                        <input type="radio" name="my-accordion-2" />
+                        <div class="collapse-title text-xl font-medium">
+                            Show raw playlist data
+                        </div>
+                        <div class="collapse-content">
+                            <pre>{{ playlists }}</pre>
+                            <p class="text-error">You see? No <code>playlist.is_album</code> anywhere.</p>
+                        </div>
                     </div>
                 </div>
 
                 <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Users" />
 
                 <div role="tabpanel" class="tab-content" v-if="users.length != 0">
-                    <p><br>This things is bugged.<br><br>But like really.</p>
                     <div v-for="user in users">
-                        <!--UserCard :userParsedData="user" /-->
+                        <UserCard :userParsedData="user" />
                     </div>
                 </div>
             </div>
