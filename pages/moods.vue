1e16f5f879b8d893316c4cfd02471ee5874e8b8c
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-11 19:24:42 +0200

    🚧 Working on icon set

--- pages/moods.vue
+++ pages/moods.vue
@@ -6,5 +6,30 @@
         <p class="mt-4">Use what works instead ⬇️</p>
         <SearchBull placeholderText="I bet you I work." />
 
+        <h1 class="text-center text-6xl font-black my-10 ">
+            Mood Icons
+        </h1>
+        <h3 class="text-center text-2xl font-black my-1 ">
+            <Icon name="ph:leaf-fill" /> - XXX
+            <br>
+            <Icon name="ph:fire-fill" /> - Rage Enabled
+            <br>
+            <Icon name="ic:baseline-water-drop" /> - Chillin'
+            <br>
+            <Icon name="streamline:snow-flake-solid" /> - Frozen
+            <br>
+            <Icon name="fluent:dust-28-filled" /> - Strange
+            <br>
+            <Icon name="ph:moon-fill" /> - Vibin'
+            <br>
+            <Icon name="streamline:flash-2-solid" /> - Energized
+            <br>
+            <Icon name="streamline:bomb-solid" /> - Explosive
+            <br>
+            <Icon name="codicon:sparkle-filled" /> - XXX
+            <br>
+            <Icon name="fluent:weather-duststorm-24-filled" />
+            <!--Icon name="mdi:weather-dust" /--> - XXX
+        </h3>
     </div>
 </template>
