aa8bc9143464700a26d93b07143b9894540d1a22
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-13 17:48:18 +0200

    🎨 MOOD is a rainbow. Of moods. Of you(s)

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,7 @@
 <template>
     <footer class="footer footer-center p-10 bg-primary text-primary-content pb-32 mt-4 border-t-2 border-base-300">
         <aside>
-            <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="500.000000pt" height="500.000000pt"
-                viewBox="0 0 500.000000 500.000000" preserveAspectRatio="xMidYMid meet" class="fill-base-300 w-32 h-32">
-
-                <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)" stroke="none">
-                    <path d="M1110 2491 l0 -1361 1390 0 1390 0 0 513 c0 281 3 894 7 1361 5 637
-3 848 -5 845 -10 -4 -302 -289 -1144 -1118 l-248 -244 -375 369 c-207 203
--519 510 -695 683 l-320 313 0 -1361z" />
-                </g>
-            </svg>
+            <img src="/favicon.png" class="w-28 h-28" />
             <p class="font-bold">
                 MOOD is <span class="bg-gradient-to-r from-warning to-error bg-clip-text text-transparent">Open Source</span><br>
                 <span class="text-center">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'icon', href: "/favicon-dark.svg" },
+        { rel: 'icon', href: "/favicon.png" },
         { rel: 'manifest', href: "/manifest.json" }
       ],
       meta: [
